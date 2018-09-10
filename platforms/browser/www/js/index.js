1456af7fbc0e0a77d3b40665526ff4022e32a04d
ls-sys <ls-sys.phonegap@outlook.com>
2018-09-10 16:33:22 -0600

    ver 0.9.15r20 test fcm

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -449,6 +449,15 @@ $$(document).on('deviceready', function()
 			alert(token);
 		});
 		
+		FCMPlugin.onNotification(function(data){
+			if(data.wasTapped){
+			  //Notification was received on device tray and tapped by the user.
+			  alert( JSON.stringify(data) );
+			}else{
+			  //Notification was received in foreground. Maybe the user needs to be notified.
+			  alert( JSON.stringify(data) );
+			}
+		});
 		console.log(AppVersion.version);
 		console.log(AppVersion.build); 
 		$$("#chipVercion").html("Ver: " + AppVersion.version);
