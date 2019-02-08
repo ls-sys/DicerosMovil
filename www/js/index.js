aa3aaa22b62cb34737b1786ca7a015b83039bba1
ls-sys <ls-sys.phonegap@outlook.com>
2019-02-08 17:08:49 -0600

    ver 1.11.0r13 modificanes menores

--- www/js/index.js
+++ www/js/index.js
@@ -498,8 +498,8 @@ $$(document).on('deviceready', function()
 
 		var notificationOpenedCallback = function(jsonData) 
 		{
-			alert(jsonData["notificationID"]);
-			alert(jsonData["title"] + "\n" + jsonData["body"]);
+			alert(jsonData["notification"]["payload"]["notificationID"]);
+			alert(jsonData["notification"]["payload"]["title"] + "\n" + jsonData["notification"]["payload"]["body"]);
 			alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
 		};
 
